32f0b13657d6e2792ba315951642761d238b2f69
verbose <ev3rnub@gmail.com>
2025-09-17 18:29:40 -0700

    added code.js, w/javascript logic, and style.css to style game.html to satisfy Assignment 2.2: The Building Blocks of JavaScript - Functions

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ Creation Date: September 17, 2025
  
 ## 09/17/2025:
 * 1. Created Skeleton and Readme/initial commit.
+* 2. Added a javascript function to increment the displayed points on game.html.
+* 3. Added another javascript function to add to the number of clones if there is enough points. 
+* 4. Added styles.css to style the game.html page.
---
+++ code.js
@@ -0,0 +1,39 @@
+function updatePoints(){
+  const displayedPoints = document.getElementById('points');
+
+  //Read from the data attribute (always a string)
+  const current = Number(displayedPoints.dataset.points) || 0; // dataset.points → "0"
+
+  //Increment
+  const newValue = current + 1;
+
+  //Write back to both the attribute and the visible text
+  displayedPoints.dataset.points = newValue;
+  displayedPoints.innerHTML = newValue;
+}
+
+function buyClone(){
+    const displayedPoints = document.getElementById('points');
+    const displayedClones = document.getElementById('clones');
+    //Read from the data attribute (always a string)
+    const currentPoints = Number(displayedPoints.dataset.points) || 0; // dataset.points → "0"
+    const currentClones = Number(displayedClones.dataset.points) || 0;
+    //Decrement
+    if (currentPoints >= 5){
+        //points
+        const newPointsValue = currentPoints - 5;
+        //Write back to both the attribute and the visible text
+        displayedPoints.dataset.points = newPointsValue;
+        displayedPoints.innerHTML = newPointsValue;
+        //clones
+        const newCloneValue = currentClones + 1
+        displayedClones.dataset.points = newCloneValue;
+        displayedClones.innerHTML = newCloneValue;
+    }else{
+        displayedClones.innerHTML = "NEED 5 POINTS"
+        setTimeout(() => clonesEl.innerHTML = clones, 1500);
+    }
+    
+
+
+}
